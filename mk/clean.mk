6799f9692de33373ac81ae54681383aab4eb9d8d
Beat Richartz <beat.richartz@gmail.com>
2015-11-29 18:22:13 +0000

    more specific clean

--- mk/clean.mk
+++ mk/clean.mk
@@ -7,3 +7,17 @@ clean:
 	rm -f $(TEST_BIN_DIR)/$(TEST_TARGET)
 	rm -f $(TEST_BIN_DIR)/$(TEST_TARGET)_debug
 	@echo "Cleaned $(TARGET)"
+
+clean_$(TARGET):
+	rm -f $(OBJECTS)
+	rm -f $(BIN_DIR)/$(TARGET)
+
+clean_$(TEST_TARGET):
+	rm -f $(TEST_OBJECTS)
+	rm -f $(TEST_BIN_DIR)/$(TEST_TARGET)
+	rm -f $(TEST_BIN_DIR)/$(TEST_TARGET)_debug
+	@echo "Cleaned $(TEST_TARGET)"
+
+clean_test_runner:
+	rm -f $(TEST_RUNNER_OBJECTS)
+	@echo "Cleaned test runner"
